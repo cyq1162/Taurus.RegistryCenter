063ff39aa892d4caf95e9839cb3958094929ea58
路过秋天 <272657997@qq.com>
2024-01-22 16:13:54 +0800

    1

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taurus.Mvc;
+
+namespace Taurus.RegistryCenter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls(MvcConfig.Kestrel.Urls);
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
